73e818017ba4e34b0a80aaa9e1fd3dd373b65e8c
Darren Burns <darrenb900@gmail.com>
2023-04-18 13:50:50 +0100

    Skeleton with sidebar and introspection

---
+++ textual_click/introspect.py
@@ -0,0 +1,83 @@
+from typing import Dict, Any
+
+import click
+
+
+def introspect_click_app(app: click.Group) -> Dict[str, Any]:
+    """
+        Introspect a Click application and build a data structure containing
+        information about all commands, options, arguments, and subcommands.
+
+        This function recursively processes each command and its subcommands
+        (if any), creating a nested dictionary that includes details about
+        options, arguments, and subcommands.
+
+        Args:
+            app (click.Group): The Click application's top-level group instance.
+
+        Returns:
+            Dict[str, Any]: A nested dictionary containing the Click application's
+            structure, including information about commands, options, arguments,
+            and subcommands. The dictionary has the following structure:
+
+            {
+                'command_name': {
+                    'options': [
+                        {
+                            'name': str,
+                            'type': str,
+                            'default': Any,
+                            'help': Optional[str]
+                        },
+                        ...
+                    ],
+                    'arguments': [
+                        {
+                            'name': str,
+                            'type': str,
+                            'required': bool
+                        },
+                        ...
+                    ],
+                    'subcommands': {
+                        'subcommand_name': { ... },
+                        ...
+                    }
+                },
+                ...
+            }
+        """
+    data: Dict[str, Any] = {}
+
+    def process_command(cmd_name: str, cmd_obj: click.Command) -> Dict[str, Any]:
+        cmd_data: Dict[str, Any] = {
+            'options': [],
+            'arguments': [],
+            'subcommands': {}
+        }
+
+        for param in cmd_obj.params:
+            if isinstance(param, click.Option):
+                cmd_data['options'].append({
+                    'name': param.name,
+                    'type': param.type.name,
+                    'default': param.default,
+                    'help': param.help
+                })
+            elif isinstance(param, click.Argument):
+                cmd_data['arguments'].append({
+                    'name': param.name,
+                    'type': param.type.name,
+                    'required': param.required
+                })
+
+        if isinstance(cmd_obj, click.core.Group):
+            for subcmd_name, subcmd_obj in cmd_obj.commands.items():
+                cmd_data['subcommands'][subcmd_name] = process_command(subcmd_name, subcmd_obj)
+
+        return cmd_data
+
+    for cmd_name, cmd_obj in app.commands.items():
+        data[cmd_name] = process_command(cmd_name, cmd_obj)
+
+    return data
--- textual_click/textual_click.py
+++ textual_click/textual_click.py
@@ -1,13 +1,53 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Label
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any, Text
+
+import click
+from rich.text import TextType
+from textual.app import ComposeResult, App
+from textual.containers import VerticalScroll
+from textual.widgets import Pretty, Tree
+from textual.widgets.tree import TreeNode
+
+from textual_click.introspect import introspect_click_app
+
+
+class CommandTree(Tree):
+    def __init__(self, label: TextType, cli_metadata: dict[str, Any]):
+        super().__init__(label)
+        self.show_root = False
+        self.guide_depth = 2
+        self.cli_metadata = cli_metadata
+
+    def on_mount(self):
+        def build_tree(data: dict[str, Any], node: TreeNode) -> TreeNode:
+            for cmd_name, cmd_data in data.items():
+                if cmd_data["subcommands"]:
+                    child = node.add(cmd_name)
+                    build_tree(cmd_data["subcommands"], child)
+                else:
+                    node.add_leaf(cmd_name)
+            return node
+
+        build_tree(self.cli_metadata, self.root)
+        self.root.expand_all()
+        self.select_node(self.root)
 
 
 class TextualClick(App):
+    CSS_PATH = Path(__file__).parent / "textual_click.scss"
+
+    def __init__(self, cli: click.Group) -> None:
+        super().__init__()
+        self.cli = cli
+        self.cli_metadata = introspect_click_app(cli)
 
     def compose(self) -> ComposeResult:
-        yield Label("Hello, world!")
-
-
-app = TextualClick()
-if __name__ == '__main__':
-    app.run()
+        tree = CommandTree("--", self.cli_metadata)
+        tree.focus()
+        yield tree
+        yield VerticalScroll(
+            Pretty(self.cli_metadata),
+            id="home-body",
+        )
